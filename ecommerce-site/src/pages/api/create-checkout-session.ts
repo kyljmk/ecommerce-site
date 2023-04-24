4bd4b410d69be2711791ce2c9e99dc59300357b9
kyljmk <k.mansonkullin@googlemail.com>
2023-04-24 23:03:36 +0200

    Add create-checkout-session Method

--- ecommerce-site/src/pages/api/create-checkout-session.ts
+++ ecommerce-site/src/pages/api/create-checkout-session.ts
@@ -1,3 +1,4 @@
+import { IProduct } from "@/Types"
 import {Request, Response} from "express"
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
@@ -5,6 +6,34 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 export default async (req: Request, res: Response) => {
     const {items, email } = req.body
 
-    console.log(items)
-    console.log(email)
+    const transformedItems = items.map((item: IProduct) => ({
+
+        quantity: 1,
+        price: {
+            currency: "gbp",
+            product_data: {
+                name: item.title,
+                images: [item.image]
+            },
+            unit_amount: item.price * 100
+        }
+    }))
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        shipping_options: ["shr_1N0W2KKWKoO69dwVdT14k0i2"],
+        shipping_address_collection: {
+            allowed_countries: ["GB", "US", "CA", "SE"]
+        },
+        line_items: transformedItems,
+        mode: "payment",
+        success_url: `${process.env.HOST}/success`,
+        cancel_url: `${process.env.HOST}/checkout`,
+        metadata: {
+            email,
+            images: JSON.stringify(items.map((item: IProduct) => item.image))
+        }
+    })
+
+    res.status(200).json({id: session.id})
 }
--- ecommerce-site/src/pages/checkout.tsx
+++ ecommerce-site/src/pages/checkout.tsx
@@ -27,6 +27,14 @@ function Checkout() {
       items,
       email: session?.user?.email,
     });
+
+    const result = await stripe?.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result?.error) {
+      alert(result.error.message);
+    }
   };
 
   return (
