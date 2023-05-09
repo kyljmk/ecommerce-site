db97850e253a47d33102fc3af5c698dae2b97b9c
kyljmk <k.mansonkullin@googlemail.com>
2023-05-09 22:37:26 +0200

    Add Firestore Config

--- ecommerce-site/firebase.ts
+++ ecommerce-site/firebase.ts
@@ -1,4 +1,6 @@
-import Firebase from "firebase/app"
+import firebase from "firebase/app"
+import {getFirestore} from "firebase/firestore"
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyD6l9wdASZIqsJnBdoDhQWDl08Swo-BBYo",
@@ -8,3 +10,9 @@ const firebaseConfig = {
     messagingSenderId: "376984579580",
     appId: "1:376984579580:web:aec8ffd7f984540f0f9679"
   };
+
+const app = !firebase.getApps().length ? firebase.initializeApp(firebaseConfig) : firebase.getApp()
+
+const db = getFirestore(app)
+
+export default db
--- ecommerce-site/src/pages/orders.tsx
+++ ecommerce-site/src/pages/orders.tsx
@@ -1,7 +1,52 @@
+import Header from "@/components/Header";
+import { GetServerSideProps } from "next";
+import { getSession, useSession } from "next-auth/react";
 import React from "react";
+import db from "../../firebase";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
-function Orders() {
-  return <div>Orders</div>;
+
+function Orders({ orders }: any) {
+  const session = useSession();
+  return (
+    <div>
+      <Header />
+
+      <main className="max-w-screen-xl mx-auto p-10">
+        <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
+          Your Orders
+        </h1>
+        {session ? (
+          <h2>x Orders</h2>
+        ) : (
+          <h2>Please sign in to see your orders</h2>
+        )}
+        <div className="mt-5 space-y-4"></div>
+      </main>
+    </div>
+  );
 }
 
 export default Orders;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+  const session = await getSession(context)
+
+  if(!session){
+    return {
+      props:{}
+    }
+  }
+
+  const docRef = doc(db, "users")
+  const usersSnpshot = await getDoc(usersCol)
+
+  const a = usersSnpshot.docs.filter(a => a.)
+
+  // const citiesCol = collection(db, 'cities');
+  // const citySnapshot = await getDocs(citiesCol);
+  // const cityList = citySnapshot.docs.map(doc => doc.data());
+
+}
